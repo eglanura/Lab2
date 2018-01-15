3d9ef28206b8dc406962edc0c06250d64950a1d3
Egla Nura <eglanura@trentu.ca>
2018-01-15 18:35:04 -0500

    This is the second lab

---
+++ src/egla/Nura.java
@@ -0,0 +1,17 @@
+package egla;
+
+public class Nura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+a = 2;
+b=3;
+
+c = a+b;
+
+System.out.println(c);
+
+	}
+
+}
